372f628c04ff426527d39c5febf07b39c4d33ce7
bruna darri <bruna.darri@aluno.senai.br>
2026-04-24 12:09:41 -0300

    fazendo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import './App.css';
 import Header from './componentes/Header';
 import styled from 'styled-components';
+import Pesquisa from './componentes/Pesquisa';
 
-//container que guarda estilos (styled components )
+// Container que guarda estilos (styled components)
 const AppContainer = styled.div`
-  text-align: center;
-  width: 100vw;
-  height: 100vw;
-  background-image: linear-gradient(90deg, #002f52 35%, #326589);
-
-
-li {
-  list-style: none;
-}
+    width: 100vw;
+    height: 100vh;
+    background-image: linear-gradient(90deg, #002F52 35%, #326589);
 `
-
 
 function App() {
   return (
-      <AppContainer>
-      <Header/>
-      </AppContainer>
+    <AppContainer>
+      <Header />
+      <Pesquisa />
+    </AppContainer>
   );
 }
 
