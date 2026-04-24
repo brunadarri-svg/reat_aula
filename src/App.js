ad5bd1dc8f9898bba890e78dd530916929c78418
bruna darri <bruna.darri@aluno.senai.br>
2026-04-24 09:43:55 -0300

    terminando

--- src/App.js
+++ src/App.js
@@ -2,22 +2,25 @@ import './App.css';
 import Header from './componentes/Header';
 import styled from 'styled-components';
 
-const AppContainer = '
+//container que guarda estilos (styled components )
+const AppContainer = styled.div`
+  text-align: center;
   width: 100vw;
   height: 100vw;
   background-image: linear-gradient(90deg, #002f52 35%, #326589);
 
 
-.li {
-  list-style:none;
+li {
+  list-style: none;
 }
-'
+`
+
 
 function App() {
   return (
-    <div className="App">
-     <Header/>
-    </AppContainer>
+      <AppContainer>
+      <Header/>
+      </AppContainer>
   );
 }
 
--- src/componentes/Logo/index.js
+++ src/componentes/Logo/index.js
@@ -1,12 +1,19 @@
-import './estilo.css';
 import logo from '../../imagens/logo.svg';
+import styled from 'styled-components';
 
+const LogoImag= styled.img`
+    margin-right: 10px;
+`
+const LogoContainer=styled.div`
+  display: flex;
+  font-size: 30px;
+`
 function Logo() {
     return (
-        <div className='logo'>
-            <img src={logo} alt='logo' className='logo-img'></img>
+        <LogoContainer>
+            <LogoImag src={logo} alt='logo' ></LogoImag>
             <p><strong>SENAI Books</strong></p>
-        </div>
+        </LogoContainer>
     )
 }
 
