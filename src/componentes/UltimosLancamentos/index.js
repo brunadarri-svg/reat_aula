6acedc6c228380d0bc2e7751757fdd096550cc23
bruna darri <bruna.darri@aluno.senai.br>
2026-04-29 10:53:38 -0300

    fazendo

--- src/componentes/UltimosLancamentos/index.js
+++ src/componentes/UltimosLancamentos/index.js
@@ -1,40 +1,43 @@
-import { livros } from "./dadosUltimosLancamentos"
-import styled  from "styled-components"
+import { livros } from "./dadosUltimosLancamentos";
+import styled from "styled-components";
+import { Titulo } from "../Título";
+import CardRecomenda  from "../CardRecomenda";
+import imagemLivro from'../../imagens/livro2.png';
 
-const UltimosLancamentosContainer= styled.section`
-background-color: #EBECEE;
-padding-bottom:20px;
-display: flex;
-flex-direction:colum;
+const UltimosLancamentosContainer = styled.section`
+    background-color: #EBECEE;
+    padding-bottom: 20px;
+    display: flex;
+    flex-direction: column;
 `
 
-const Titulo= styled.h2`
-width:100%;
-padding: 30px 0;
-background-color: #FFF;
-color:#EB9b00;
-font-size:36px;
-text-align: center;
-margin:0;
+const NovosLivrosContainer = styled.div`
+    margin-top: 30px;
+    display: flex;
+    width: 100%;
+    justify-content:center;
+    cursor: pointer;
 `
-const NovosLivrosContainer= styled.div`
-margin-top:30px;
-display:flex;
-width:100%;
-justify-content:center;
-cursor:pointer;
-`
-
 
 function UltimosLancamentos(){
     return(
         <UltimosLancamentosContainer>
-        <Titulo>ÚLTIMOS LANÇAMENTOS</Titulo>
+        <Titulo 
+        cor={"#000"}
+        tamanhoFonte={"36px"}
+        >
+            ÚLTIMOS LANÇAMENTOS</Titulo>
         <NovosLivrosContainer>
-            { livros.map( livro => (
+        { livros.map( livro => (
             <img src={livro.src}/>
         ))}
         </NovosLivrosContainer>
+        <CardRecomenda
+           titulo="Talvez você se interesse por..."
+           subtitulo="Angular 11"
+           descricao="Construindo uma aplicação com a plataforma Google."
+           img={imagemLivro}
+        />
         </UltimosLancamentosContainer>
     )
 }
